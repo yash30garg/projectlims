9687053c3aba7407673f58f39db46463036c3b56
Anirudh Srinath <m1042946@mindtree.com>
2017-12-29 15:02:44 +0530

    Pushing Footer

--- src/app-component/footer-component/footer.jsx
+++ src/app-component/footer-component/footer.jsx
@@ -3,46 +3,133 @@ import './footer.css'
 import $ from 'jquery';
 class Footer extends Component {
 
-    componentDidMount()
-    {
-         $(".container").css("background-color", ' #60003a');
-         $(".footer-copyright").css("background-color", ' #60003a');
-$(".page-footer").css("background-color", ' #60003a');
-         
-         
-    }
-
+    
     render()
     {
         return(
-<div className="fixed">
-    <footer className="page-footer">
-          <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">About Mindtree Library</h5>
-                <p className="grey-text text-lighten-4">For more details....</p>
-              </div>
-              <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Links</h5>
-                <ul>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="footer-copyright">
-            <div>
-            © 2017 Copyright 
-            {/*<a class="grey-text text-lighten-4 right" href="#!">More Links</a>*/}
-            </div>
-          </div>
-        </footer>
-    </div>
 
+<div>
+
+
+<footer class="mt-5">
+  <div class="container-fluid bg-faded mt-5">
+    <div class="container">
+      <div class="row py-3">
+        {/*<!-- footer column 1 start -->*/}
+        <div class="col-md-4">
+          {/*<!-- row start -->*/}
+          <div class="row py-2">
+            <div class="col-sm-3 hidden-md-down">
+              <a class="bg-circle bg-info" href="https://twitter.com/ ">
+                <i class="fa fa-2x fa-fw fa-twitter" aria-hidden="true "></i>
+              </a>
+            </div>
+            <div class="col-sm-9">
+              <h4>Tweets</h4>
+              Embed here?
+            </div>
+          </div>
+          {/*<!-- row end -->*/}
+        </div>
+        {/*<!-- footer column 1 end -->*/}
+        {/*<!-- footer column 2 start -->*/}
+        <div class="col-md-4">
+          {/*<!-- row start -->*/}
+          <div class="row py-2">
+            <div class="col-sm-3 hidden-md-down">
+              <a class="bg-circle bg-info" href="#">
+                <i class="fa fa-2x fa-fw fa-address-card" aria-hidden="true "></i>
+              </a>
+            </div>
+            <div class="col-sm-9">
+              <h4>Contact us</h4>
+              <p>Why not?</p>
+            </div>
+          </div>
+          {/*<!-- row end -->*/}
+          {/*<!-- row start -->*/}
+          <div class="row py-2">
+            <div class="col-sm-3 hidden-md-down">
+              <a class="bg-circle bg-info" href="#">
+                <i class="fa fa-2x fa-fw fa-laptop" aria-hidden="true "></i>
+              </a>
+            </div>
+            <div class="col-sm-9">
+              <h4>Cookie policy</h4>
+              <p class=" ">We use <a class=" " href="/# ">cookies </a></p>
+            </div>
+          </div>
+          {/*<!-- row end -->*/}
+        </div>
+        {/*<!-- footer column 2 end -->*/}
+        {/*<!-- footer column 3 start -->*/}
+        <div class="col-md-4">
+          {/*<!-- row starting  -->*/}
+          <div class="row py-2">
+            <div class="col-sm-3 hidden-md-down">
+              <a class="bg-circle bg-danger" href="# ">
+                <i class="fa fa-2x fa-fw fa-file-pdf-o" aria-hidden="true "></i>
+              </a>
+            </div>
+            <div class="col-sm-9">
+              <h4>Download pdf</h4>
+              <p> You like print?
+                
+                </p>
+
+            </div>
+          </div>
+          {/*<!-- row ended -->*/}
+          {/*<!-- row starting  -->*/}
+          <div class="row py-2">
+            <div class="col-sm-3 hidden-md-down">
+              <a class="bg-circle bg-info" href="https://twitter.com/ ">
+                <i class="fa fa-2x fa-fw fa-info" aria-hidden="true "></i>
+              </a>
+            </div>
+            <div class="col-sm-9">
+              <h4>Info</h4>
+              About us.
+            </div>
+          </div>
+          {/*<!-- row ended -->*/}
+        </div>
+        {/*<!-- footer column 3 end -->*/}
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container-fluid bg-primary py-3">
+    <div class="container">
+      <div class="row py-3">
+        <div class="col-md-9">
+          <p class="text-white">Lorem ipsum.</p>
+        </div>
+        <div class="col-md-3">
+          <div class="d-inline-block">
+            <div class="bg-circle-outline d-inline-block">
+              <a href="https://www.facebook.com/" class="text-white"><i class="fa fa-2x fa-fw fa-facebook"></i>
+		</a>
+            </div>
+
+            <div class="bg-circle-outline d-inline-block">
+              <a href="https://twitter.com/" class="text-white">
+                <i class="fa fa-2x fa-fw fa-twitter"></i></a>
+            </div>
+
+            <div class="bg-circle-outline d-inline-block">
+              <a href="https://www.linkedin.com/company/" class="text-white">
+                <i class="fa fa-2x fa-fw fa-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+  </div>
 
         )
     }
