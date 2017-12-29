f43bf57e2e270ba31ffdb50d2634a12f9cd681b6
Chaitanya Boyapati <M1043195@mindtree.com>
2017-12-29 15:18:56 +0530

    Solved Hamburger Problem

--- src/app-component/main-component/admin-component/admin.jsx
+++ src/app-component/main-component/admin-component/admin.jsx
@@ -1,7 +1,10 @@
 import React, {Component}from 'react';
 import Header from './../../header-component/header';
 import Footer from '../../footer-component/footer.jsx';
+import Pbooks from '../admin-component/PreferredBooks/PrefferdBooks.jsx';
+import { BorrowedSlider } from '../user-component/borrowedBooks/borrowedSlider';
 // import SearchResults from './SearchResults'
+
 export default class Admin extends Component
 {
     render()
@@ -9,9 +12,9 @@ export default class Admin extends Component
         return(
             <div>
             <Header/>
-            {/*<Pbooks/>*/}
-            {/*<BorrowedSlider/>*/}
-            {/*<Footer />*/}
+            <Pbooks/>
+            <BorrowedSlider/>
+            <Footer />
             </div>
         );
     }
