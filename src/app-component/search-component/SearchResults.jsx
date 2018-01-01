76e7716140c3644aedd546c08d59e377a6140e03
Aishwarya Sinha <m1042929@mindtree.com>
2018-01-01 09:28:02 +0530

    pulling again

--- src/app-component/search-component/SearchResults.jsx
+++ src/app-component/search-component/SearchResults.jsx
@@ -1,55 +1,107 @@
-import React,{Component} from 'react';
-import { processedData } from './Search';
+import React, {Component} from 'react';
+import {processedData} from './Search';
 import $ from 'jquery';
+import axios from 'axios';
+let users,books;
 class SearchResults extends Component
- {
-     constructor(props)
-     {
-     super(props);
-    }
- 
-     render()
-     {
-                 $(function() {
-    $('.card').hover(
-        function() {
-            $(this).find('> .card-image > img.activator').click();
-        }, function() {
-            $(this).find('> .card-reveal > .card-title').click();
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            output: []
         }
-    );
-});
+    }
 
+    componentDidMount()
+    {
 
-         const a=processedData.map(res=>{
-             return(
-                     <div class="card col s6 m3">
-    <div class=" card-image waves-effect waves-block waves-light">
-      <img class="activator" src={res.details.url} alt="not available" width="250" height="200" />
-    </div>
-    <div className="card-content">
+        axios
+            .get('https://api.myjson.com/bins/ds48n')
+            .then(res => {
+                this.setState({output: res.data});
+                users = this.state.output;
+        const b = users.filter((res) => res.user.mid === "1042948")
+        books=b[0].userBooks.length
+            });
+    }
+    request() {
+        if (books <5) {
+            books++;
+            alert("The Requested Book has been allotted to you..Please Collect It from the Library");
+        }
+        else{
+            alert("Oops..Looks like You cannot borrow more books. Please return a book to borrow more")
+        }
+    }
+    render()
+    {
 
-        <b>Title: </b><span>{res.details.title}</span><br/>
-        <b>Category: </b><span>{res.details.category}</span>
-    </div>
-    <div className="card-reveal">
-      <span className="card-title grey-text text-darken-4">Book Details<i className="material-icons right">close</i></span>
-        <b>ISBN: </b><span>{res.isbn}</span><br/>
-        <b>Author: </b><span>{res.details.author}</span><br/>
-        <b>Publisher: </b><span></span>{res.details.publisher}<br/>
-        <b>Rating: </b><span>{res.details.rating}</span><br/>
-        <b>Copies available: </b><span>{res.details.copies}</span>
-    </div>
-  </div>
-             );
-         })
+        $(function () {
+            $('.card')
+                .hover(function () {
+                    $(this)
+                        .find('> .card-image > img.activator')
+                        .click();
+                }, function () {
+                    $(this)
+                        .find('> .card-reveal > .card-title')
+                        .click();
+                });
+        });
 
-         return(
-             <div className="container row">
-             {a}
-             </div>
-         );
-     }
-    
- }
- export default SearchResults;
+        const a = processedData.map(res => {
+            return (
+                <div className="card col-md-5 ml-5 mt-5">
+                    <div className=" card-img-top">
+                        <img
+                            className="container-fill m-4"
+                            src={res.details.url}
+                            alt="not available"
+                            height="200"/>
+                    </div>
+                    <div className="card-block">
+                        <div className="card-title">
+                            <h4>
+                                <b>Title:
+                                </b>
+                                <span>{res.details.title}</span>
+                            </h4>
+                        </div>
+                        <div className="card-text">
+                            <b>Category:
+                            </b>
+                            <span>{res.details.category}</span>
+                            <br/>
+                            <b>ISBN:
+                            </b>
+                            <span>{res.isbn}</span><br/>
+                            <b>Author:
+                            </b>
+                            <span>{res.details.author}</span><br/>
+                            <b>Publisher:
+                            </b>
+                            <span></span>{res.details.publisher}<br/>
+                            <b>Rating:
+                            </b>
+                            <span>{res.details.rating}</span><br/>
+                            <b>Copies available:
+                            </b>
+                            <span>{res.details.copies}</span>
+                        </div>
+                        <br/>
+                        <button type="button" class="btn btn-primary" onClick={this.request}>Request</button>
+                    </div>
+                </div>
+            );
+        })
+        return (
+            <div className="container-fill">
+                <div className="row offset-md-1">
+                    {a}
+                </div>
+            </div>
+        )
+    }
+}
+export default SearchResults;
