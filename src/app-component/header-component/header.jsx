8db59eadc04327354ff92bb501be486cef83d161
Aishwarya Sinha <m1042929@mindtree.com>
2018-01-01 23:02:20 +0530

    search bar updated

--- src/app-component/header-component/header.jsx
+++ src/app-component/header-component/header.jsx
@@ -1,40 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../../logo.svg';
 import '../App.css';
-// import Search from '../search-component/Search';
-// import './header.css';
-// import PBooks from '../main-component/admin-component/PreferredBooks/PrefferdBooks.jsx';
-//import Footer from '../footer-component/footer.jsx';
-
-import { Link } from 'react-router-dom';
+// import Search from '../search-component/Search'; import './header.css';
+// import PBooks from
+// '../main-component/admin-component/PreferredBooks/PrefferdBooks.jsx'; import
+// Footer from '../footer-component/footer.jsx';
+import {Link} from 'react-router-dom';
+export var key;
 class Header extends Component {
-
+handle(){
+  key=document.getElementById("key").value
+  console.log(key)
+}
   render() {
-
 
     return (
       <div>
-
         <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            class="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <a ><img className="App-logo" src={"https://www.mindtree.com/themes/custom/mindtree_theme/logo.svg"} alt="My logo" align="left" /></a>
+          <a ><img
+            className="App-logo"
+            src={"https://www.mindtree.com/themes/custom/mindtree_theme/logo.svg"}
+            alt="My logo"
+            align="left"/></a>
           <a class="navbar-brand" href="#">LiMS</a>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="col-lg-9">
+          <div className="row offset-md-3">
+              <div class="input-group">
+                <input type="text" id="key" class="form-control" placeholder="Search for..."/>
+                <span class="input-group-btn">
+                  <Link to="/Search">
+                    <button class="btn btn-primary" onClick={this.handle} type="button">Go!</button>
+                  </Link>
+                </span>
+              </div>
+            </div>
+            </div>
             <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <Link to="/Search">
-                  <a class="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
-                </Link>
-              </li>
+
+              <li class="nav-item active"></li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Wishlist  </a>
+                <a class="nav-link" href="#">Wishlist
+                </a>
               </li>
 
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Profile </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="http://example.com"
+                  id="navbarDropdownMenuLink"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false">
+                  Profile
+                </a>
+                <div
+                  class="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="navbarDropdownMenuLink">
                   <a class="dropdown-item" href="#">Preferences</a>
                   <a class="dropdown-item" href="#">Settings</a>
                   <div class="dropdown-divider"></div>
@@ -46,7 +78,6 @@ class Header extends Component {
         </nav>
 
       </div>
-
 
     )
   }
--- src/app-component/search-component/SearchResults.jsx
+++ src/app-component/search-component/SearchResults.jsx
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import {processedData} from './Search';
 import $ from 'jquery';
-<<<<<<< HEAD
 import axios from 'axios';
-let users,books;
-=======
 import './Search.css';
->>>>>>> c22a16639ba2c3e5aa6c9e52d698e6f6f3f1c3d8
+let users,books;
 class SearchResults extends Component
 {
     constructor(props)
@@ -19,8 +16,6 @@ class SearchResults extends Component
 
     componentDidMount()
     {
-
-<<<<<<< HEAD
         axios
             .get('https://api.myjson.com/bins/ds48n')
             .then(res => {
@@ -57,8 +52,8 @@ class SearchResults extends Component
 
         const a = processedData.map(res => {
             return (
-                <div className="card col-md-5 ml-5 mt-5">
-                    <div className=" card-img-top">
+                <div className="card col-md-5 mt-5 h-100">
+                    <div className="card-img-top">
                         <img
                             className="container-fill m-4"
                             src={res.details.url}
@@ -101,8 +96,8 @@ class SearchResults extends Component
             );
         })
         return (
-            <div className="container-fill">
-                <div className="row offset-md-1">
+            <div className="container-fluid">
+                <div className="row">
                     {a}
                 </div>
             </div>
@@ -110,37 +105,4 @@ class SearchResults extends Component
     }
 }
 export default SearchResults;
-=======
-         const a=processedData.map(res=>{
-             return(
-                 <div className="col-md-6 col-sm-12 col-lg-3 each">
-        <div className="card" style={{ width: '20rem' }}>
-      <img className="card-img-top" src={res.details.url} alt="not available" height="200vh"/>
-    <div className="card-block text-block">
-        <b>Title: </b><span>{res.details.title}</span><br/>
-        <b>Category: </b><span>{res.details.category}</span>
-    </div>
-  </div>
-  </div>
-             );
-
-//     <div class="card" style="width: 20rem;">
-//   <img class="card-img-top" src="..." alt="Card image cap">
-//   <div class="card-block">
-//     <h4 class="card-title">Card title</h4>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="btn btn-primary">Go somewhere</a>
-//   </div>
-// </div>
-         })
-
-         return(
-             <div className="row eachRow">
-             {a}
-             </div>
-         );
-     }
-    
- }
- export default SearchResults;
->>>>>>> c22a16639ba2c3e5aa6c9e52d698e6f6f3f1c3d8
+        
