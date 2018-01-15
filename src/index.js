f1ac892f21bb34df7db2a833fa3847dd90ef23be
Yash Garg <m1043117@mindtree.com>
2018-01-15 12:05:47 +0530

    pulling changes

--- src/index.js
+++ src/index.js
@@ -12,11 +12,12 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 import '../node_modules/bootstrap/scss/bootstrap.scss';
-var UserDetails = JSON.parse(localStorage.getItem('limsuser'))
-let mid=UserDetails.userName.split("@");
-let res=mid[0].split("M")
-// alert(res[1])
-window.user=res[1];
+// var UserDetails = JSON.parse(localStorage.getItem('limsuser'));
+// console.log(UserDetails)
+// let mid=UserDetails.userName.split("@");
+// let res=mid[0].split("M")
+// // alert(res[1])
+// window.user=res[1];
 // alert(window.user);
 function getData(){
         axios
