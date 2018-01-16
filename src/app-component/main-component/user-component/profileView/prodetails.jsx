ab81942833e109ed088b533c8ebd273ec991081c
Chaitanya Boyapati <M1043195@mindtree.com>
2018-01-16 12:38:44 +0530

    warnings in pro

--- src/app-component/main-component/user-component/profileView/prodetails.jsx
+++ src/app-component/main-component/user-component/profileView/prodetails.jsx
@@ -15,71 +15,71 @@ class Profile extends Component {
     <div>
 <Header />
 
-<div class="container-fluid" style={{backgroundColor : "#FFF8DC", minHeight : "33px"}} >
+<div className="container-fluid" style={{backgroundColor : "#FFF8DC", minHeight : "33px"}} >
   <br/><nr />
-	<div class="innerwrap">
-		<section class="section1 clearfix">
+	<div className="innerwrap">
+		<section className="section1 clearfix">
 			<div>
-				<div class="row grid clearfix">
-					<div class="col2 first">
+				<div className="row grid clearfix">
+					<div className="col2 first">
 						<h1>{JSON.parse(localStorage.getItem('limsuser')).profile.name}</h1>
 						<p>{JSON.parse(localStorage.getItem('limsuser')).profile.unique_name}</p>
 						
 					</div>
-					<div class="col2 last">
-						<div class="grid clearfix">
-							<div class="col3 first">
+					<div className="col2 last">
+						<div className="grid clearfix">
+							<div className="col3 first">
 								<h1>{JSON.parse(localStorage.getItem('bbooks')).length}</h1>
 								<span>Books Borrowed</span>
 							</div>
-							<div class="col3"><h1>9</h1>
+							<div className="col3"><h1>9</h1>
 							<span>Wishlist</span></div>
-							<div class="col3 last"><h1>12</h1>
+							<div className="col3 last"><h1>12</h1>
 							<span>Books Requested</span></div>
 						</div>
 					</div>
 				</div>
-				<div class="container" style={{minHeight : "33px"}}>
-					<ul class="row2tab clearfix">
-						<li><i class="fa fa-list-alt"></i> My Books </li>
-						<li><i class="fa fa-heart"></i> My WishList </li>
-						<li><i class="fa fa-check"></i> My Requests </li>
-						<li><i class="fa fa-thumbs-o-up "></i> Reviews </li>
+				<div className="container" style={{minHeight : "33px"}}>
+					<ul className="row2tab clearfix">
+						<li><i className="fa fa-list-alt"></i> My Books </li>
+						<li><i className="fa fa-heart"></i> My WishList </li>
+						<li><i className="fa fa-check"></i> My Requests </li>
+						<li><i className="fa fa-thumbs-o-up "></i> Reviews </li>
 					</ul>
 				</div>
 			</div>
-			<span class="smalltri">
+			<span className="smalltri">
 				
-			<i class="fa fa-star"></i>
+			<i className="fa fa-star"></i>
 			</span>
 		</section>
-		<section class="section2 clearfix">
-			<div class="grid">
-				<div class="col3 first">
-					<div class="postcont"><img src="http://www.ohmyindia.com/wp-content/uploads/2017/09/dhoni8.jpg" alt="" />
+		<section className="section2 clearfix">
+			<div className="grid">
+				<div className="col3 first">
+					<div className="postcont"><img src="http://www.ohmyindia.com/wp-content/uploads/2017/09/dhoni8.jpg" alt="" />
 					</div>
-					<div class="profileinfo">
+					<div className="profileinfo">
 						<img src="http://images.contactmusic.com/newsimages/david_beckham_1133321.jpg" alt="" />
 						<p>MS Dhoni has hit most sixes for India in international cricket</p>
-						<span>Read more <i class="fa fa-angle-right"></i></span>
+						<span>Read more <i className="fa fa-angle-right"></i></span>
 					</div>
 				</div>
-				<div class="col3 center">
-					<div class="postcont"><img src="https://d1u4oo4rb13yy8.cloudfront.net/article/67350-wcbqbtpugy-1504272183.jpg" alt="" />
+				<div className="col3 center">
+					<div className="postcont"><img src="https://d1u4oo4rb13yy8.cloudfront.net/article/67350-wcbqbtpugy-1504272183.jpg" alt="" />
 					</div>
-					<div class="profileinfo">
+					<div className="profileinfo">
 						<img src="https://d1u4oo4rb13yy8.cloudfront.net/article/67350-wcbqbtpugy-1504272183.jpg" alt="" />
 						<p>MS Dhoni has been involved in most T20 wins</p>
-						<span>Read more <i class="fa fa-angle-right"></i></span>
+						<span>Read more <i className="fa fa-angle-right"></i></span>
 					</div>
 				</div>
-				<div class="col3 last">
-					<div class="postcont"><img src="http://ste.india.com/sites/default/files/2017/07/06/607698-ms-dhoni-pc-pti4.jpg" alt="" />
+				<div className="col3 last">
+					<div className="postcont"><img src="http://ste.india.com/sites/default/files/2017/07/06/607698-ms-dhoni-pc-pti4.jpg" alt="" />
 					</div>
-					<div class="profileinfo">
+					<div className="profileinfo">
 						<img src="http://images.contactmusic.com/newsimages/david_beckham_1133321.jpg" alt="" />
 						<p>MS Dhoni has captained most number of intl. matches (334)</p>
-						<span>Read more <i class="fa fa-angle-right"></i></span>
+						<span>Read more <i className="fa fa-angle-right"></i></span>
 					</div>
 				</div>
 			</div>
