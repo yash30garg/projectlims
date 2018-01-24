e24b47bc34b9a4facd94a329cf97e2014c6d6d1a
Anirudh Srinath <m1042946@mindtree.com>
2018-01-24 15:18:30 +0530

    updated category

--- src/app-component/main-component/admin-component/adminheader.jsx
+++ src/app-component/main-component/admin-component/adminheader.jsx
@@ -7,7 +7,7 @@ render()
 {
 
 return (
-                <nav class="navbar navbar-toggleable-md navbar-default" style={{backgroundColor:'#A9A9A9'}}>
+                <nav class="navbar navbar-toggleable-md navbar-default" style={{backgroundColor:'#7e4a35'}}>
                     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation"   >
                         <span class="navbar-toggler-icon"></span></button>
                     <button
