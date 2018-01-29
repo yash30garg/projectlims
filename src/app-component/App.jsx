d1f7f6145eacc200f4e4f2d0e3f5dbeed241bbf8
Chaitanya Boyapati <M1043195@mindtree.com>
2018-01-29 14:52:33 +0530

    fetch Boorowed Books

--- src/app-component/App.jsx
+++ src/app-component/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 // import Search from './search-component/Search'
 import SearchResults from './search-component/SearchResults'
@@ -6,8 +6,8 @@ import SearchResults from './search-component/SearchResults'
 // import SearchResults from '../app-component/search-component/SearchResults.jsx';
 // import BootHeader from '../app-component/header-component/bootheader.jsx';
 // import Pages from '../app-component/main-component/admin-component/adminDashboard/pages.jsx';
-import {BookDetails} from '../app-component/BookDetails-Component/bookDetails'
-import DashBoard from  '../app-component/main-component/admin-component/adminDashboard/dashboard.jsx';
+import { BookDetails } from '../app-component/BookDetails-Component/bookDetails'
+import DashBoard from '../app-component/main-component/admin-component/adminDashboard/dashboard.jsx';
 import Login from './main-component/login-component/login.jsx';
 import storeBbooks from '../state/store/storeBbooks';
 import { HashRouter, Route, Switch } from 'react-router-dom';
@@ -28,127 +28,126 @@ export var user_name;
 var req = require('request');
 // let res;
 // let users;
-window.display='';
+window.display = '';
 class App extends Component {
   constructor() {
     super();
-    var Backlen=window.history.length;  
+    var Backlen = window.history.length;
     // storeBbooks.dispatch({type:"STORE_BBOOKS",payload: []}) 
-     window.history.go(-Backlen);
-     this.state={
-       bbooks:[],
-       display:[],
-       wishlist:[]
-     }
+    window.history.go(-Backlen);
+    this.state = {
+      bbooks: [],
+      display: [],
+      wishlist: []
+    }
   }
-  
-getBorrowedData(){
-fetch('http://localhost:3005/borrowedBooks/getBooks',{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body:JSON.stringify({
-              mid:window.user
-            })
-})
-.then((res)=>res.json())
-.then((res)=>{
-  // storeBbooks.dispatch({type:"STORE_BBOOKS",payload: res.data[0]})
-  // alert("dispatch")
-  this.setState({
-    bbooks:res.data[0],
-  wishlist:res.data[1]
-  })
-  // console.log(res.data[1])
-  // window.bbooks=res.data.borrowedbooks;
-  // console.log(window.bbooks);
-})
-}
 
-  addUser=(UserDetails)=>{
-    fetch('http://localhost:3005/user/addUser',{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            // form:{mid:"1042932"}
-            body:JSON.stringify({
-                name:UserDetails.profile.name,
-                email:UserDetails.userName,
-                mid:window.user,
-                role:"user",
-                borrowedbooks:[],
-                wishlist:[]
-            })
+  getBorrowedData() {
+    fetch('http://localhost:3005/borrowedBooks/getBooks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        mid: window.user
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // storeBbooks.dispatch({type:"STORE_BBOOKS",payload: res.data[0]})
+        // alert("dispatch")
+        this.setState({
+          bbooks: res.data[0],
+          wishlist: res.data[1]
         })
-        .then((res)=>res.json())
-        .then((res)=>{
-          console.log(res)
-          localStorage.setItem('token',res.token)
-          localStorage.setItem('role',res.user[0].role)
-          fetch('http://localhost:3005/books/getBooks',
-        {
-        method:'GET',
-        headers:{'Content-Type': 'application/json',
-          'Authorization': res.token
+        // console.log(res.data[1])
+        // window.bbooks=res.data.borrowedbooks;
+        // console.log(window.bbooks);
+      })
+  }
+
+  addUser = (UserDetails) => {
+    fetch('http://localhost:3005/user/addUser', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // form:{mid:"1042932"}
+      body: JSON.stringify({
+        name: UserDetails.profile.name,
+        email: UserDetails.userName,
+        mid: window.user,
+        role: "user",
+        borrowedbooks: [],
+        wishlist: []
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        localStorage.setItem('token', res.token)
+        localStorage.setItem('role', res.user[0].role)
+        fetch('http://localhost:3005/books/getBooks',
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': res.token
+            }
+          })
+          .then((response) => response.json())
+          .then((response) => {
+            console.log("booksssssss");
+            this.setState({
+              display: response
+            })
+
+          })
+        if (res.status === "Exists") {
+          this.getBorrowedData();
         }
       })
-      .then((res)=>res.json())
-      .then((res)=>{
-  console.log("booksssssss");
-  this.setState({
-    display:res
-  })
-  console.log(res);
-
-    })
-          if(res.status==="Exists"){
-          this.getBorrowedData();
-          }
-        })
   }
-  componentWillMount()
-    {
-      var user=authContext._user;
-      let mid=user.userName.split("@");
-      let id=mid[0].split("M")
-      // alert(res[1])
-      window.user=id[1];
-      this.addUser(user)
-  //     fetch('http://localhost:3005/books/getBooks',
-  //     {
-  //       method:'GET',
-  //       headers:{'Content-Type': 'application/json',
-  //         'Authorization': localStorage.getItem('token')
-  //       }
-  //     })
-  //     .then((res)=>res.json())
-  //     .then((res)=>{
-  // console.log("booksssssss");
-  // this.setState({
-  //   display:res
-  // })
-  // console.log(res);
+  componentWillMount() {
+    var user = authContext._user;
+    let mid = user.userName.split("@");
+    let id = mid[0].split("M")
+    // alert(res[1])
+    window.user = id[1];
+    this.addUser(user)
+    //     fetch('http://localhost:3005/books/getBooks',
+    //     {
+    //       method:'GET',
+    //       headers:{'Content-Type': 'application/json',
+    //         'Authorization': localStorage.getItem('token')
+    //       }
+    //     })
+    //     .then((res)=>res.json())
+    //     .then((res)=>{
+    // console.log("booksssssss");
+    // this.setState({
+    //   display:res
+    // })
+    // console.log(res);
 
-  //   })
-}
+    //   })
+  }
 
   render() {
     // alert("app")
-    window.wishlist=this.state.wishlist;
+    window.wishlist = this.state.wishlist;
     // window.bbooks=this.state.bbooks;
-    window.display=this.state.display;
-    storeBbooks.dispatch({type:"STORE_BBOOKS",payload:this.state.bbooks})
+    window.display = this.state.display;
+    storeBbooks.dispatch({ type: "STORE_BBOOKS", payload: this.state.bbooks })
     // alert(window.display.length)
     console.log(authContext._user.profile.given_name);
     user_name = authContext._user.profile.given_name;
     localStorage.setItem('limsuser', JSON.stringify(authContext._user))
-    localStorage.setItem('user-name',JSON.stringify(authContext._user.profile.given_name))
+    localStorage.setItem('user-name', JSON.stringify(authContext._user.profile.given_name))
     console.log(localStorage.getItem('limsuser'))
     console.log(localStorage.getItem('adal.access.token.keyfa61fc30-ea79-4d93-8038-65273b42c71c'))
     // let value ="Bearer" + localStorage.getItem('adal.access.token.keyfa61fc30-ea79-4d93-8038-65273b42c71c')
     // console.log(`https://graph.microsoft.com/beta/me/photo/${value}`)
     var UserDetails = JSON.parse(localStorage.getItem('limsuser'))
     // this.getData();
-    localStorage.setItem('mid',window.user)
-    
+    localStorage.setItem('mid', window.user)
+
     // alert(window.user);
     //  axios.get('https://api.myjson.com/bins/14x90j')
     //  .then(res=>{
@@ -172,10 +171,10 @@ fetch('http://localhost:3005/borrowedBooks/getBooks',{
       <HashRouter basename="/">
         <div className="App" id="App">
           <Switch>
-            <Route path="/login" exact component={Login}/>
-            <Route path="/" exact component={User}/>
-            <Route path="/search" exact component={SearchResults}/>
-            <Route path="/search/details" exact component={BookDetails}/>
+            <Route path="/login" exact component={Login} />
+            <Route path="/" exact component={User} />
+            <Route path="/search" exact component={SearchResults} />
+            <Route path="/search/details" exact component={BookDetails} />
             {/*<Route path="/pdetails" exact component={ProductDetails}/>*/}
             {/*<Route path="/admin" exact component={AdminLogin} />  */}
             <Route path="/adminbooks" exact component={BookAdmin} />
@@ -186,8 +185,8 @@ fetch('http://localhost:3005/borrowedBooks/getBooks',{
             <Route path="/contactus" exact component={ContactUs} />
             <Route path="/bookadd" exact component={BookAdd} />
             <Route path="/bookedit" exact component={BookEdit} />
-            
-            
+
+
           </Switch>
 
         </div>
