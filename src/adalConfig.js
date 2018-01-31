39065e0ba6074c4dcbdb07b3451860538d515bee
Anirudh Srinath <m1042946@mindtree.com>
2018-01-31 12:09:26 +0530

    pull

--- src/adalConfig.js
+++ src/adalConfig.js
@@ -33,30 +33,30 @@
 import AuthenticationContext from 'adal-angular';
 
 // To login through http://localhost:3000, use this adalConfig variable
-// const config = {
-//   instance: 'https://login.microsoftonline.com/',
-//   tenant: '85c997b9-f494-46b3-a11d-772983cf6f11',
-//   clientId: 'fa61fc30-ea79-4d93-8038-65273b42c71c',
-//   redirectUri:'http://localhost:3000/#/',
-//   postLogoutRedirectUri: 'http://localhost:3000/#/login',
-//   cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost
-//   endpoints: {
-//     graphApiUri: "https://graph.microsoft.com"
-//   }
-// };
-
-// To login through https://limsreact.azurewebsites.net/#/, use this adalConfig variable
 const config = {
   instance: 'https://login.microsoftonline.com/',
   tenant: '85c997b9-f494-46b3-a11d-772983cf6f11',
-  clientId: '859c085f-f3d7-40d9-8994-cfebc8407fde',
-  redirectUri:'https://limsreact.azurewebsites.net/#/',
-  postLogoutRedirectUri: 'https://limsreact.azurewebsites.net/#/login',
+  clientId: 'fa61fc30-ea79-4d93-8038-65273b42c71c',
+  redirectUri:'http://localhost:3000/#/',
+  postLogoutRedirectUri: 'http://localhost:3000/#/login',
   cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost
   endpoints: {
     graphApiUri: "https://graph.microsoft.com"
   }
 };
+
+// To login through https://limsreact.azurewebsites.net/#/, use this adalConfig variable
+// const config = {
+//   instance: 'https://login.microsoftonline.com/',
+//   tenant: '85c997b9-f494-46b3-a11d-772983cf6f11',
+//   clientId: '859c085f-f3d7-40d9-8994-cfebc8407fde',
+//   redirectUri:'https://limsreact.azurewebsites.net/#/',
+//   postLogoutRedirectUri: 'https://limsreact.azurewebsites.net/#/login',
+//   cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost
+//   endpoints: {
+//     graphApiUri: "https://graph.microsoft.com"
+//   }
+// };
 
 export const authContext = new AuthenticationContext(config);
 
