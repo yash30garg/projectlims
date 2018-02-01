fad790137c80e6dba1e29f0cc194793fafc11f5d
Chaitanya Boyapati <M1043195@mindtree.com>
2018-02-01 18:49:57 +0530

    azure

--- src/app-component/isLoggedIn.js
+++ src/app-component/isLoggedIn.js
@@ -1,32 +1,9 @@
-export var requireAuth = (path) => {
-
-      if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
-        {
-            if(path==="http://localhost:3000/#/login") {
-                window.location = "http://localhost:3000/#/"
-            }
-            else {
-                // console.log(JSON.parse(localStorage.getItem('limsuser')))
-                console.log("yes")
-                // console.log(path)
-                window.location = path
-            }
-        }
-        else
-        {
-            console.log("No")
-            // console.log(path)
-            window.location.replace("http://localhost:3000/#/login")
-            // window.locaton = "http://localhost:3000/#/login"
-        }
-}
-
 // export var requireAuth = (path) => {
 
 //       if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
 //         {
-//             if(path==="https://limsreact.azurewebsites.net/#/login") {
-//                 window.location = "https://limsreact.azurewebsites.net/#/"
+//             if(path==="http://localhost:3000/#/login") {
+//                 window.location = "http://localhost:3000/#/"
 //             }
 //             else {
 //                 // console.log(JSON.parse(localStorage.getItem('limsuser')))
@@ -39,7 +16,30 @@ export var requireAuth = (path) => {
 //         {
 //             console.log("No")
 //             // console.log(path)
-//             window.location.replace("https://limsreact.azurewebsites.net/#/login")
+//             window.location.replace("http://localhost:3000/#/login")
 //             // window.locaton = "http://localhost:3000/#/login"
 //         }
 // }
+
+export var requireAuth = (path) => {
+
+      if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
+        {
+            if(path==="https://limsreact.azurewebsites.net/#/login") {
+                window.location = "https://limsreact.azurewebsites.net/#/"
+            }
+            else {
+                // console.log(JSON.parse(localStorage.getItem('limsuser')))
+                console.log("yes")
+                // console.log(path)
+                window.location = path
+            }
+        }
+        else
+        {
+            console.log("No")
+            // console.log(path)
+            window.location.replace("https://limsreact.azurewebsites.net/#/login")
+            // window.locaton = "http://localhost:3000/#/login"
+        }
+}
