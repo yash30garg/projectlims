3c1da61bdbea30c1e949d47581adf7f21cd100a4
Yash Garg <m1043117@mindtree.com>
2018-02-02 15:06:49 +0530

    pulling

--- src/app-component/isLoggedIn.js
+++ src/app-component/isLoggedIn.js
@@ -1,41 +1,15 @@
-// export var requireAuth = (path) => {
-
-//       if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
-//         {
-//             if(path==="http://localhost:3000/#/login") {
-//                 window.login="yes"
-//                 window.location = "http://localhost:3000/#/"
-//             }
-//             else {
-//                 // console.log(JSON.parse(localStorage.getItem('limsuser')))
-//                 // console.log("yes")
-//                 window.login="yes"
-//                 // console.log(path)
-//                 window.location = path
-//             }
-//         }
-//         else
-//         {
-//             // console.log("No")
-//             window.login="no"
-//             // console.log(path)
-//             window.location.replace("http://localhost:3000/#/login")
-//             // window.locaton = "http://localhost:3000/#/login"
-//         }
-// }
-
 export var requireAuth = (path) => {
 
       if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
         {
-            if(path==="https://limsreact.azurewebsites.net/#/login") {
-                 window.login="yes"
-                window.location = "https://limsreact.azurewebsites.net/#/"
+            if(path==="http://localhost:3000/#/login") {
+                window.login="yes"
+                window.location = "http://localhost:3000/#/"
             }
             else {
                 // console.log(JSON.parse(localStorage.getItem('limsuser')))
                 // console.log("yes")
-                 window.login="yes"
+                window.login="yes"
                 // console.log(path)
                 window.location = path
             }
@@ -43,9 +17,35 @@ export var requireAuth = (path) => {
         else
         {
             // console.log("No")
-             window.login="no"
+            window.login="no"
             // console.log(path)
-            window.location.replace("https://limsreact.azurewebsites.net/#/login")
+            window.location.replace("http://localhost:3000/#/login")
             // window.locaton = "http://localhost:3000/#/login"
         }
 }
+
+// export var requireAuth = (path) => {
+
+//       if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
+//         {
+//             if(path==="https://limsreact.azurewebsites.net/#/login") {
+//                  window.login="yes"
+//                 window.location = "https://limsreact.azurewebsites.net/#/"
+//             }
+//             else {
+//                 // console.log(JSON.parse(localStorage.getItem('limsuser')))
+//                 // console.log("yes")
+//                  window.login="yes"
+//                 // console.log(path)
+//                 window.location = path
+//             }
+//         }
+//         else
+//         {
+//             // console.log("No")
+//              window.login="no"
+//             // console.log(path)
+//             window.location.replace("https://limsreact.azurewebsites.net/#/login")
+//             // window.locaton = "http://localhost:3000/#/login"
+//         }
+// }
