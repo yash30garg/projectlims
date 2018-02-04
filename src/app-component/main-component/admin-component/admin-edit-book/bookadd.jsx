9b31fb318ee9c318bd26695a75a1493cb55a65b5
Anirudh Srinath <m1042946@mindtree.com>
2018-02-04 23:33:01 +0530

    add book fix

--- src/app-component/main-component/admin-component/admin-edit-book/bookadd.jsx
+++ src/app-component/main-component/admin-component/admin-edit-book/bookadd.jsx
@@ -10,6 +10,8 @@ import { requireAuth } from '../../../isLoggedIn.js'
 // import '../../../../../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
 
 import {addBook} from '../../../mongo/addBook'
+import DashBoardStats from '../admin-stats/adminstats'
+
 
 
 import AdminHeader from '../adminheader'
@@ -106,35 +108,7 @@ class BookAdd extends Component {
                         <div className="row">
                             <div className="col-md-3">
 
-                                <div className="list-group">
-                                    <a href="!#" class="list-group-item active">
-                                        <span class="fa fa-cog" aria-hidden="true" ></span>  DashBoard</a>
-                                    <a href="!#" class="list-group-item  list-group-item-action"> <span class="fa fa-list-alt" aria-hidden="true"></span>Total Books<div className='mov' style={{ paddingRight: "170px" }} /><span class="badge  badge-pill badge-warning">100</span></a>
-                                    <a href="!#" class="list-group-item  list-group-item-action"> <span class="fa fa-pencil" aria-hidden="true"></span>Books available<div className='mov' style={{ paddingRight: "150px" }} /><span className="badge badge-pill badge-warning">75</span></a>
-                                    <a href="!#" class="list-group-item  list-group-item-action"> <span class="fa fa-user" aria-hidden="true"></span>Users<div className='mov' style={{ paddingRight: "227px" }} /><span className="badge badge-pill badge-warning mov">{count}</span></a>
-                                    {/*<a href="!#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>*/}
-                                </div>
-
-                                <br />
-                                <div className="card">
-                                    <div className="card-header card-primary"><div className="t">
-                                        <span className="fa fa-list" aria-hidden="true"></span> Books Stats
-                                      </div></div>
-                                    <h6 className="he5">Books Available :</h6>
-                                    <div className="p1">
-                                        <div class="progress">
-                                            <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
-                                        </div>
-                                    </div>
-
-                                    <h6 className="he6"> Books to be returned :</h6>
-                                    <div className="p2">
-                                        <div className="progress">
-                                            <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
-                                        </div>
-                                    </div>
-                                </div>
-
+                                <DashBoardStats />
 
                             </div>
 
