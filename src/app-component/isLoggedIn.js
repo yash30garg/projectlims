ba80d368c2e640ea0c7e50d40824474fbb7d104b
Anirudh Srinath <m1042946@mindtree.com>
2018-02-04 23:36:28 +0530

    fix

--- src/app-component/isLoggedIn.js
+++ src/app-component/isLoggedIn.js
@@ -1,28 +1,28 @@
-export var requireAuth = (path) => {
+// export var requireAuth = (path) => {
 
-      if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
-        {
-            if(path==="http://localhost:3000/#/login") {
-                window.login="yes"
-                window.location = "http://localhost:3000/#/"
-            }
-            else {
-                // console.log(JSON.parse(localStorage.getItem('limsuser')))
-                // console.log("yes")
-                window.login="yes"
-                // console.log(path)
-                window.location = path
-            }
-        }
-        else
-        {
-            // console.log("No")
-            window.login="no"
-            // console.log(path)
-            window.location.replace("http://localhost:3000/#/login")
-            // window.locaton = "http://localhost:3000/#/login"
-        }
-}
+//       if(localStorage.getItem('limsuser')!==null&&localStorage.getItem('adal.token.renew')!=="")
+//         {
+//             if(path==="http://localhost:3000/#/login") {
+//                 window.login="yes"
+//                 window.location = "http://localhost:3000/#/"
+//             }
+//             else {
+//                 // console.log(JSON.parse(localStorage.getItem('limsuser')))
+//                 // console.log("yes")
+//                 window.login="yes"
+//                 // console.log(path)
+//                 window.location = path
+//             }
+//         }
+//         else
+//         {
+//             // console.log("No")
+//             window.login="no"
+//             // console.log(path)
+//             window.location.replace("http://localhost:3000/#/login")
+//             // window.locaton = "http://localhost:3000/#/login"
+//         }
+// }
 
 export var requireAuth = (path) => {
 
